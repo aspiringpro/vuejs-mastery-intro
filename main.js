d7db4aa63034699f98cd7dd157a8cc085a776f82
MarcusReed <marcusjamesreed@gmail.com>
2019-06-22 15:02:53 -0700

    End of Lesson 2

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks'
+    product: 'Socks',
+    description: 'A warm fuzzy pair of socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
   }
 })
