27af19dafe931b2405f1258f9b33e594a7f176e2
MarcusReed <marcusjamesreed@gmail.com>
2019-06-22 15:34:38 -0700

    End of Lesson 4

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 var app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    product: 'Socks',
-    description: 'A warm fuzzy pair of socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    product: "Socks",
+    description: "A warm fuzzy pair of socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    link:
+      "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
     inventory: 10,
-    onSale: false
+    onSale: false,
+    details: ["80% cotton", "20% polyester", "Gender-Neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ],
+    sizes: ["small", "medium", "large"]
   }
-})
+});
